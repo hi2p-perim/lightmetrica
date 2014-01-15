325f2a94448c30ec5a6f1849e2a09e1138fe0804
Hisanari Otsu <hi2p.perim@gmail.com>
2014-01-15 10:38:37 +0900

    WIP implementing qbvh scene

--- src/libnanon/qbvhscene.cpp
+++ src/libnanon/qbvhscene.cpp
@@ -60,9 +60,21 @@ public:
 private:
 
 	// Build a part of QBVH.
-	// [#begin, #end) : Range of primitive indices
-	// parent : 
+	// [begin, end) is the range of primitive indices.
+	// #parent indicates the index of the parent node (specify -1 for building root node)
+	// and #child indicates the index of the child node relative to the node specified by #parent.
 	void Build(const QBVHBuildData& data, unsigned int begin, unsigned int end, int parent, int child, int depth);
+
+	// Determine the split axis and the position
+	// Returns false if the split is failed because the primitive bound is degenerated
+	bool SplitAxisAndPosition(const QBVHBuildData& data, unsigned int begin, unsigned int end, int& axis, Math::Float& splitPosition);
+
+	// Rearrange primitives by partition according to the split axis and position
+	// splitTriIndex is the boundary primitive index
+	void PartitionPrimitives(const QBVHBuildData& data, unsigned int begin, unsigned int end, int axis, Math::Float splitPosition, unsigned int& splitTriIndex);
+
+	void CreateLeafNode(int parent, int child, unsigned int begin, unsigned int end, const AABB& bound);
+	void CreateIntermediateNode(int parent, int child, const AABB& bound);
 
 private:
 
@@ -152,13 +164,131 @@ bool QBVHScene::Impl::Build()
 
 void QBVHScene::Impl::Build( const QBVHBuildData& data, unsigned int begin, unsigned int end, int parent, int child, int depth )
 {
+	// Bound of the primitives [begin, end)
+	AABB bound;
+	for (int i = begin; i < end; i++)
+	{
+		bound = bound.Union(data.triBounds[triIndices[i]]);
+	}
+	
 	// Leaf node
 	if (end - begin <= maxTriInLeaf)
 	{
-		
+		CreateLeafNode(begin, end, parent, child, bound);
+		return;
 	}
 
+	// Determine the split axis and position
+	int axis;
+	Math::Float splitPosition;
+	if (!SplitAxisAndPosition(data, begin, end, axis, splitPosition))
+	{
+		// The primitive bound is degenerated -> create a leaf node
+		CreateLeafNode(begin, end, parent, child, bound);
+		return;
+	}
+
+	// Partition primitives in [begin, end) according to split axis and position
+	unsigned int splitTriIndex;
+	PartitionPrimitives(data, begin, end, axis, splitPosition, splitTriIndex);
+
+	// Index of the current and child nodes
+	// The value is changed according to the depth of the recursion
+	int current, left, right;
+	if (depth % 2 == 1)
+	{
+		// The process in the depth is the intermediate process creating the node indexed by #parent.
+		// In the stage the function is responsible to create two sibling nodes.
+		// For example if child = 2,
+		// + parent			<- A node indexed by #current, which is created in the parent process
+		//   + child 0
+		//   + child 1
+		//   + child 2		<- A node indexed by #left
+		//   + child 3		<- A node indexed by #right
+		// The process focuses on separating the primitives in [begin, end) to child 2 and 3.
+		current = parent;
+		left = child;
+		right = child + 1;
+	}
+	else
+	{
+		// The process in the depth focus on creating intermediate node (i.e. a parent of at most four child nodes)
+		// And left and right node index specifies the starting point of the further split of the sibling two nodes.
+		// For example
+		// + parent			<- Newly created node indexed by currentNodeIndex
+		//   + child 0		<- A node indexed by #left, which means in the child call the child 0 and 1 are processed
+		//   + child 1
+		//   + child 2      <- A node indexed by #right, which means in the child call the child 2 and 3 are processed
+		//   + child 3
+		// The process focuses on separating the primitives in [begin, end) to child {0, 1} and child {2, 3}.
+		//CreateIntermediateNode();
+		left = 0;
+		right = 2;
+	}
+
+	// Process recursively
+	Build(data, begin, splitTriIndex, current, left, depth + 1);
+	Build(data, splitTriIndex, end, current, right, depth + 1);
+}
+
+bool QBVHScene::Impl::SplitAxisAndPosition( const QBVHBuildData& data, unsigned int begin, unsigned int end, int& axis, Math::Float& splitPosition )
+{
+	// Choose the axis to split
+	AABB centroidBound;
+	for (int i = begin; i < end; i++)
+	{
+		centroidBound = centroidBound.Union(data.triBoundCentroids[triIndices[i]]);
+	}
+	axis = centroidBound.LongestAxis();
+
+	// Check if the bound is degenerated
+	if (centroidBound.min[axis] == centroidBound.max[axis])
+	{
+		// Degenerated
+		return false;
+	}
+
+	// Simply use mean index
+	// TODO : Replace with bucket sorted SAH optimized split
+	splitPosition = data.triBoundCentroids[(end - begin) / 2][axis];
+	return true;
+}
+
+void QBVHScene::Impl::PartitionPrimitives( const QBVHBuildData& data, unsigned int begin, unsigned int end, int axis, Math::Float splitPosition, unsigned int& splitTriIndex )
+{
+	splitTriIndex = begin;
+	for (unsigned int i = begin; i < end; i++)
+	{
+		const unsigned int triIndex = triIndices[i];
+		if (data.triBoundCentroids[triIndex][axis] <= splitPosition)
+		{
+			// Swap indices
+			triIndices[i] = triIndices[splitTriIndex];
+			triIndices[splitTriIndex] = triIndex;
+			splitTriIndex++;
+		}
+	}
+}
+
+void QBVHScene::Impl::CreateLeafNode( int parent, int child, unsigned int begin, unsigned int end, const AABB& bound )
+{
+	// If the #parent is -1 the root is a leaf node
+	// Note that in the case the node is yet to be created
+	if (parent < 0)
+	{
+		// Create node
+		nodes.push_back(new QBVHNode());
+		parent = 0;
+	}
+
+	// Set the value to the node
+	auto& node = nodes[parent];
 	
+}
+
+void QBVHScene::Impl::CreateIntermediateNode( int parent, int child, const AABB& bound )
+{
+
 }
 
 bool QBVHScene::Impl::Intersect( Ray& ray, Intersection& isect ) const
